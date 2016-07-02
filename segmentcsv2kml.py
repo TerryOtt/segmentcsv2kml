b6de302d43f1deb478d7f84cf0ca9bdcc4b65d6e
Terry Ott <Terry.Ott@pobox.com>
2016-07-02 15:28:24 -0400

    Added ignore file

--- segmentcsv2kml.py
+++ segmentcsv2kml.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
+import sys
 import argparse
 
 def main():
+    kmlFilename = parseArgs()
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description="Convert CSV file with segment data to KML")
+    parser.add_argument('csvURL', help='URL to the CSV to download, e.g. "http://db.slickbox.net/states/Ohio-sl.csv")
 
 
 if __name__ == "__main__":
