6bf4b8ac67bc317f590486b254e08cb1b3178e88
Terry Ott <Terry.Ott@pobox.com>
2016-07-02 20:58:32 -0400

    DOne for the night

--- segmentcsv2kml.py
+++ segmentcsv2kml.py
@@ -7,24 +7,34 @@ import contextlib
 import StringIO
 import csv
 import re
+import kmldom
+import kmlengine
 
 def main():
-    csvURL = parseArgs()
+    (csvURL, kmlFile) = parseArgs()
 
     segmentList = validateAndReadCsvUrl(csvURL)
 
     print "Read {0} data lines from {1}".format(len(segmentList), csvURL)
 
-    generateKml(segmentList)
+    # Create dictionary sorted by segment type
+    segmentsByType = createSegmentTypeDictionary(segmentList)
+
+    print "Number of MH: {0}".format(len(segmentsByType['Major Highway']))
+    print "Number of mH: {0}".format(len(segmentsByType['Minor Highway']))
+
+    generateKml(segmentsByType, kmlFile)
 
 def parseArgs():
     parser = argparse.ArgumentParser(description="Convert CSV file with segment data to KML")
     parser.add_argument('csvURL', 
         help='URL to the CSV to download, e.g. "http://db.slickbox.net/states/Ohio-sl.csv"')
+    parser.add_argument('kmlFile',
+        help='KML file to generate with output')
 
     args = parser.parse_args()
 
-    return args.csvURL
+    return (args.csvURL, args.kmlFile)
 
 def validateAndReadCsvUrl(url):
     try:
@@ -59,16 +69,14 @@ def validateAndReadCsvUrl(url):
     segmentList = {}
     for row in fileLines:
         segmentDetails = {}
-        for i in range(1, len(row)):
-
-            # Add everything but ID
+        for i in range(len(row)):
             segmentDetails[columnHeaders[i]] = row[i]
 
         # Add the latitude and longitude to the segment details
         (lon, lat) = getLonLatFromPermalink(segmentDetails['PL'])
 
-        segmentDetails['Longitude'] = lon
-        segmentDetails['Latitude'] = lat
+        segmentDetails['Longitude'] = float(lon)
+        segmentDetails['Latitude'] = float(lat)
 
         # Add to list of segments with segment ID as key
         segmentList[int(row[0])] = segmentDetails
@@ -81,9 +89,122 @@ def getLonLatFromPermalink(segmentPermalink):
 
     return matches.groups()
 
-def generateKml(segmentList):
+def createSegmentTypeDictionary(segmentList):
 
-    print segmentList[83818954]
+    segmentTypeDictionary = {}
 
+    knownSegmentTypes = []
+
+    for currSegmentId in segmentList:
+        if segmentList[currSegmentId]['Road Type'] not in segmentTypeDictionary:
+            print "Found new segment type: " + segmentList[currSegmentId]['Road Type']
+            segmentTypeDictionary[segmentList[currSegmentId]['Road Type']] = [ segmentList[currSegmentId] ]
+        else:
+            segmentTypeDictionary[segmentList[currSegmentId]['Road Type']].append(
+                segmentList[currSegmentId] )
+
+    return segmentTypeDictionary
+
+
+def generateKml(segmentsByType, kmlFilename):
+
+    '''
+    Set maximums by what Google MyMaps can display
+        10 layers
+        2000 features per layer
+        max 10,000 elements per file
+
+        Retrieved from: https://support.google.com/mymaps/answer/3370982?hl=en
+    '''
+    maxKmlFeatures =            10000
+    currKmlFeatures =           0
+    maxKmlFeaturesPerLayer =    2000
+    numberOfFoldersPerType =    { 'Freeway': 0, 'Major Highway': 0, 'Minor Highway': 0 }
+    currKmlFolders =            0
+
+    kmlFactory = kmldom.KmlFactory_GetFactory()
+
+    kmlDocument = kmlFactory.CreateDocument()
+    kmlDocument.set_name('Generated KML')
+    doneProcessing = False
+
+    for currFolderType in [ 'Freeway', 'Major Highway', 'Minor Highway' ]:
+        if doneProcessing:
+            print "Found we were done processing at folder loop, bailing"
+            break
+
+        folder = kmlFactory.CreateFolder()
+        numberOfFoldersPerType[currFolderType] += 1
+        currKmlFolders += 1
+
+        currFolderName = "{0} Folder #{1}".format(currFolderType, numberOfFoldersPerType[currFolderType])
+        folder.set_name(currFolderName)
+        print "Starting folder " + currFolderName
+
+        currFeaturesInFolder = 0
+
+        if currFolderType in segmentsByType:
+            for currFeature in segmentsByType[currFolderType]:
+                if doneProcessing:
+                    print "Found we were done processing at the feature loop, bailing"
+                    break
+                placemark = kmlFactory.CreatePlacemark()
+                #placemark.set_name(currFeature['PL'])
+                placemark.set_name(currFeature['ID'])
+                placemark.set_description(str(currFeature['PL']))
+                coordinates = kmlFactory.CreateCoordinates()
+                coordinates.add_latlng(currFeature['Latitude'], currFeature['Longitude'] )
+                point = kmlFactory.CreatePoint()
+                point.set_coordinates(coordinates)
+                placemark.set_geometry(point)
+                folder.add_feature(placemark)
+
+                # Increment total features
+                currKmlFeatures += 1
+
+                if currKmlFeatures == maxKmlFeatures:
+                    print "Maxed out at 10K features"
+                    kmlDocument.add_feature(folder)
+                    currFeaturesInFolder = 0
+                    doneProcessing = True
+                    break
+                
+                # Increment features for this folder 
+                currFeaturesInFolder += 1
+
+                # Did we fill up the folder?
+                if ( currFeaturesInFolder == maxKmlFeaturesPerLayer ):
+
+                    print "Filled up folder " + currFolderName
+                    kmlDocument.add_feature(folder)
+                    currFeaturesInFolder = 0
+
+                    doneProcessing = True
+                    break
+
+                    # Do we have room for more folders?
+                    if currKmlFolders < 10:
+                        numberOfFoldersPerType[currFolderType] += 1
+
+                        folder = kmlFactory.CreateFolder()
+                        currFolderName = "{0} Folder #{1}".format(currFolderType, numberOfFoldersPerType[currFolderType])
+                        folder.set_name(currFolderName)
+
+                    else:
+                        print "Maxed out on folders, no room left"
+                        doneProcessing = True
+                        break
+
+            print "Either complete with or maxed out on " + currFolderType
+            if ( currFeaturesInFolder > 0 and not doneProcessing ):
+                kmlDocument.add_feature(folder)
+                currFeaturesInFolder = 0
+
+    with open(kmlFilename, 'w') as kmlFile:
+        kmlFile.write(
+            '<?xml version="1.0" encoding="UTF-8"?>\n<kml xmlns="http://www.opengis.net/kml/2.2">\n' +
+            kmldom.SerializePretty(kmlDocument) + '</kml>' )
+
+        
 if __name__ == "__main__":
     main()
